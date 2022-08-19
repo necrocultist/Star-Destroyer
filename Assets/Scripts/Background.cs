f0b888f655d02ac1e48e0f8439e7cbb9f6ce9c0c
karataskn20 <karataskn20@gmail.com>
2022-08-20 01:08:17 +0300

    Update Background.cs

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float BackgroundSpeed;
 
-    // Update is called once per frame
     void Update()
     {
         
