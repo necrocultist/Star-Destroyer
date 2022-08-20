436a29afb6a169352b4311ee07470d3c1f538f03
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-08-20 14:15:42 +0300

    circular

--- Assets/Scripts/CircularShoot.cs
+++ Assets/Scripts/CircularShoot.cs
@@ -6,7 +6,7 @@ public class CircularShoot : MonoBehaviour
 {
     private GameObject[] bullets;
     public GameObject bulletPrefab;
-   // private int x = 0;
+   private int x = 0;
     void Start()
     {
         StartCoroutine(SpawnCircular());
@@ -16,11 +16,13 @@ public class CircularShoot : MonoBehaviour
     {
         while (true)
         {
+            x = 0;
             bullets = new GameObject[8];
 
             for (int i = 0; i < bullets.Length; i++)
             {
-                Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                Instantiate(bulletPrefab, transform.position, Quaternion.Euler(new Vector3(0, 0, x)));
+                x += 45;
             }
             yield return new WaitForSeconds(3);
         }
