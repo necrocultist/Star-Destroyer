8b61039040f1f4caa4aac7b6805ffa4a554691d4
karataskn20 <karataskn20@gmail.com>
2022-08-20 13:53:29 +0300

    puş

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerBullet : MonoBehaviour
 {
     [SerializeField] private float playerBulletSpeed;
+    [SerializeField] private int playerBulletDamage;
 
     void FixedUpdate()
     {
@@ -14,5 +15,26 @@ public class PlayerBullet : MonoBehaviour
     void PlayerBulletMove()
     {
         transform.Translate(new Vector2(0, playerBulletSpeed * Time.fixedDeltaTime));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject gameObject = other.gameObject;
+        Debug.Log("11");
+        if (gameObject != null)
+        {
+            if (gameObject.TryGetComponent(out Health enemy))
+            {
+                enemy.DecraseHealth(playerBulletDamage);
+                if (enemy.currentHealth <= 0)
+                {
+                    enemy.isAlive = false;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("There is no gameObject this" + name + "collides.");
+        }
     }
 }
