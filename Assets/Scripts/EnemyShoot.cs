72afb745f73ba58b1494042dbc2ba976ff9278f6
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-08-20 12:41:28 +0300

    enemy shoot

---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    private bool shoot;
+    public GameObject enemy;
+    private float bulletSpawnDuration;
+    private void OnEnable()
+    {
+        shoot = true;
+        StartCoroutine(SpawnBullet());
+    }
+
+    public IEnumerator SpawnBullet()
+    {
+        while (shoot)
+        {
+            Instantiate(bullet, enemy.transform.position, quaternion.identity);
+            
+            yield return new WaitForSeconds(bulletSpawnDuration);
+        }
+    }
+}
