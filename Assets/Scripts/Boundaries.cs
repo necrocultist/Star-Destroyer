fc24093c02444767bc27eeb5d3d2c00ecfecdd06
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-08-21 12:08:17 +0300

    ekranda durma

--- Assets/Scripts/Boundaries.cs
+++ Assets/Scripts/Boundaries.cs
@@ -7,15 +7,13 @@ public class Boundaries : MonoBehaviour {
     private Vector2 screenBounds;
     private float objectWidth;
     private float objectHeight;
-
-    // Use this for initialization
+    
     void Start () {
         screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
         objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
         objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
     }
 
-    // Update is called once per frame
     void LateUpdate(){
         Vector3 viewPos = transform.position;
         viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
