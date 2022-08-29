65fa60fa472940bcb28efbedf6f40eee80786085
karataskn20 <karataskn20@gmail.com>
2022-08-29 12:49:56 +0300

    Update PlayerBullet.cs

--- Assets/Scripts/Player/PlayerBullet.cs
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -4,12 +4,14 @@ public class PlayerBullet : Bullet
 {
     protected override void OnCollisionEnter2D(Collision2D other)
     {
+        // Do something from the base method then add below
+
         GameObject gameObject = other.gameObject;
         Vector2 contactPoint = other.GetContact(0).point;
 
         if (gameObject != null)
         {
-            if (gameObject.TryGetComponent(out EnemyHealth enemy) || gameObject.TryGetComponent(out EnemySpawner _))
+            if (gameObject.TryGetComponent(out EnemyHealth enemy) || gameObject.TryGetComponent(out EnemyBulletSpawner _))
             {
                 enemy.DecraseHealth(bulletDamage);
 
